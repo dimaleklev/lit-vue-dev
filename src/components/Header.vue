d45e1eea18e0e38a44fab7857fd5f288442c8e9c
dimaleklev <62714013+dimaleklev@users.noreply.github.com>
2020-04-03 19:43:08 +0300

    Добавил router-link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <a href="http://localhost:8081/#/example1" class="ex1">Example1</a>
-        <a href="http://localhost:8081/#/example2" class="ex2">Example2</a>
+        <!-- <a href="http://localhost:8081/#/example1" class="ex1">Example1</a>
+        <a href="http://localhost:8081/#/example2" class="ex2">Example2</a> -->
+        <router-link to="/example1" class="ex1">Перейти к Example1</router-link>
+        <router-link to="/example2" class="ex2">Перейти к Example2</router-link>
     </div>
 </template>
 
@@ -12,10 +14,10 @@ export default {
 </script>
 
 <style scoped>
-    ex1{
+    .ex1{
         margin-right: 20px;
     }
     .ex2{
         margin-left: 20px;
     }
-</style>>
+</style>
